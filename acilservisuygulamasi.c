82f649cdbd55aabac3f5a79e40be9a6e56b4d896
BennuDAL <bennudal2139@gmail.com>
2025-03-14 21:01:20 +0300

    Update acilservisuygulamasi.c

--- acilservisuygulamasi.c
+++ acilservisuygulamasi.c
@@ -5,12 +5,14 @@
 
 #define MAX_HASTA 100
 #define MAX_STR 50
+#define MAX_ADRES 200
 
 // Hasta bilgilerini tutan struct
 typedef struct {
     char ad[MAX_STR];
     char soyad[MAX_STR];
     char tcNo[12];
+    char dogumTarihi[11];  // YYYY-MM-DD formatında
     int yas;
     char cinsiyet;
     char sikayet[MAX_STR*2];
@@ -18,6 +20,10 @@ typedef struct {
     char telefon[15];
     char yakinTelefon[15];
     char alerji[MAX_STR];
+    char adres[MAX_ADRES];
+    char il[MAX_STR];
+    char ilce[MAX_STR];
+    char postaKodu[6];
     int buyukTansiyon;
     int kucukTansiyon;
     float ates;
@@ -47,7 +53,7 @@ int receteCount = 0;
 
 // Poliklinik ve doktor listesi
 PoliklinikDoktor poliklinikler[] = {
-   {"Dahiliye", "Dr. Suleyman Cetin"},
+    {"Dahiliye", "Dr. Suleyman Cetin"},
     {"Ortopedi", "Dr. Sultan Pembe"},
     {"Kardiyoloji", "Dr. Beril Koru"},
     {"Noroloji", "Dr. Azra Batur"},
@@ -55,9 +61,8 @@ PoliklinikDoktor poliklinikler[] = {
     {"Genel Cerrahi", "Dr. Yusuf Can Cakmak"},
     {"Cildiye", "Dr. Mustafa Ceylan"},
     {"KBB", "Prf. Dr. Beste Yildiz"},
-     {"Pediatri", " Dr. Guney Alaca"}
+    {"Pediatri", " Dr. Guney Alaca"}
 };
-
 // Fonksiyon prototipleri
 void menuGoster();
 void yeniHastaKaydi();
@@ -67,6 +72,7 @@ void hastaSil();
 void receteOlustur();
 int rastgeleSayi(int min, int max);
 char* doktorBul(char* poliklinik);
+void hastaBilgisiHL7Format(int index);
 
 int main() {
     int secim;
@@ -103,23 +109,20 @@ int main() {
     
     return 0;
 }
-
 // Ana menu
 void menuGoster() {
     printf("\n===== ACIL SERVIS YONETIM SISTEMI =====\n");
     printf("1- Yeni hasta kaydi\n");
-    printf("2- Kayitli hastanin bilgilerini goruntule\n");
-    printf("3- Kayitli tum hastalari goruntule\n");
+    printf("2- Kayitli hastanin bilgilerini goruntule (HL7 formati)\n");
+    printf("3- Kayitli tum hastalari goruntule (HL7 formati)\n");
     printf("4- Hasta sil\n");
     printf("5- Kayitli hastaya recete olustur\n");
     printf("0- Cikis\n");
 }
-
 // Min ve max arasinda rastgele sayi uretir
 int rastgeleSayi(int min, int max) {
     return min + rand() % (max - min + 1);
 }
-
 // Poliklinik adina gore doktor bulur
 char* doktorBul(char* poliklinik) {
     int i;
@@ -133,7 +136,51 @@ char* doktorBul(char* poliklinik) {
     
     return "Dr. Nobet";  // Eger eslesen poliklinik bulunamazsa
 }
-
+// Hasta bilgilerini HL7 formatında gösterir
+void hastaBilgisiHL7Format(int index) {
+    char tarih[20];
+    time_t t = time(NULL);
+    struct tm *tm = localtime(&t);
+    strftime(tarih, sizeof(tarih), "%Y%m%d%H%M%S", tm);
+    
+    // MSH segmenti (Message Header)
+    printf("MSH|^~\\&|ACIL|HASTANE|SISMRK|HASTANE|%s||ADT^A04|MSG%d|P|2.5.1\n", tarih, rastgeleSayi(1000, 9999));
+    
+    // PID segmenti (Patient Identification)
+    printf("PID|1||%s||%s^%s||%s|%c||||%s^%s^%s^%s^%s^TR||%s|%s|||||||||||\n", 
+           hastalar[index].tcNo, 
+           hastalar[index].ad, hastalar[index].soyad, 
+           hastalar[index].dogumTarihi, 
+           hastalar[index].cinsiyet, 
+           hastalar[index].adres, hastalar[index].ilce, hastalar[index].il, hastalar[index].postaKodu, "", // Adres bilgileri
+           hastalar[index].telefon, 
+           hastalar[index].yakinTelefon);
+    
+    // PV1 segmenti (Patient Visit)
+    printf("PV1|%d|%s|%s|%d|%d|%d||||%s|||||||%s||||||||||||||||||||||||||\n", 
+           hastalar[index].siraNo, hastalar[index].poliklinik, hastalar[index].doktor, 
+           hastalar[index].triaj, hastalar[index].buyukTansiyon, hastalar[index].kucukTansiyon, 
+           hastalar[index].sikayet);
+    
+    // AL1 segmenti (Alerji bilgileri)
+    if (strlen(hastalar[index].alerji) > 0 && strcmp(hastalar[index].alerji, "Yok") != 0) {
+        printf("AL1|1|DA|^%s||||||\n", hastalar[index].alerji);
+    }
+    
+    // OBX segmenti (Observation)
+    printf("OBX|1|NM|ATES|1|%.1f|C|||||F|||%s\n", 
+           hastalar[index].ates, tarih);
+    
+    // OBX segmenti (Triaj)
+    printf("OBX|2|NM|TRIJ|1|%d|Seviye|||||F|||%s\n", 
+           hastalar[index].triaj, tarih);
+    
+    // OBX segmenti (Tansiyon)
+    printf("OBX|3|NM|TNSN|1|%d/%d|mmHg|||||F|||%s\n", 
+           hastalar[index].buyukTansiyon, hastalar[index].kucukTansiyon, tarih);
+    
+    printf("\n");
+}
 // Yeni hasta kaydi fonksiyonu
 void yeniHastaKaydi() {
     if (hastaCount >= MAX_HASTA) {
@@ -160,8 +207,7 @@ void yeniHastaKaydi() {
         printf("Hata: TC No 11 haneli olmalidir!\n");
         return;
     }
-    
-    // TC Numarasi daha once kayitli mi kontrolu
+     // TC Numarasi daha once kayitli mi kontrolu
     int i;
     for (i = 0; i < hastaCount; i++) {
         if (hastalar[i].aktif && strcmp(hastalar[i].tcNo, yeniHasta.tcNo) == 0) {
@@ -169,6 +215,9 @@ void yeniHastaKaydi() {
             return;
         }
     }
+    
+    printf("Dogum Tarihi (YYYY-MM-DD): ");
+    scanf("%s", yeniHasta.dogumTarihi);
     
     printf("Yas: ");
     scanf("%d", &yeniHasta.yas);
@@ -190,6 +239,18 @@ void yeniHastaKaydi() {
     
     printf("Alerji: ");
     scanf(" %[^\n]", yeniHasta.alerji);
+    
+    printf("Adres: ");
+    scanf(" %[^\n]", yeniHasta.adres);
+    
+    printf("Il: ");
+    scanf("%s", yeniHasta.il);
+    
+    printf("Ilce: ");
+    scanf("%s", yeniHasta.ilce);
+    
+    printf("Posta Kodu: ");
+    scanf("%s", yeniHasta.postaKodu);
     
     printf("Buyuk Tansiyon: ");
     scanf("%d", &yeniHasta.buyukTansiyon);
@@ -216,35 +277,20 @@ void yeniHastaKaydi() {
     printf("Hasta Sira No: %d\n", yeniHasta.siraNo);
     printf("Atanan Doktor: %s\n", yeniHasta.doktor);
 }
-
 // Hasta bilgilerini goruntuleme fonksiyonu
 void hastaBilgisiGoruntule() {
     char tcNo[12];
     int bulundu = 0;
     int i;
     
-    printf("\n=== HASTA BILGISI GORUNTULE ===\n");
+    printf("\n=== HASTA BILGISI GORUNTULE (HL7) ===\n");
     printf("TC No: ");
     scanf("%s", tcNo);
     
     for (i = 0; i < hastaCount; i++) {
         if (hastalar[i].aktif && strcmp(hastalar[i].tcNo, tcNo) == 0) {
-            printf("\n--- HASTA BILGILERI (HL7) ---\n");
-            printf("Ad: %s\n", hastalar[i].ad);
-            printf("Soyad: %s\n", hastalar[i].soyad);
-            printf("TC No: %s\n", hastalar[i].tcNo);
-            printf("Yas: %d\n", hastalar[i].yas);
-            printf("Cinsiyet: %c\n", hastalar[i].cinsiyet);
-            printf("Sikayet: %s\n", hastalar[i].sikayet);
-            printf("Triaj Onceligi: %d\n", hastalar[i].triaj);
-            printf("Telefon: %s\n", hastalar[i].telefon);
-            printf("Yakin Telefon: %s\n", hastalar[i].yakinTelefon);
-            printf("Alerji: %s\n", hastalar[i].alerji);
-            printf("Tansiyon: %d/%d\n", hastalar[i].buyukTansiyon, hastalar[i].kucukTansiyon);
-            printf("Ates: %.1f\n", hastalar[i].ates);
-            printf("Poliklinik: %s\n", hastalar[i].poliklinik);
-            printf("Doktor: %s\n", hastalar[i].doktor);
-            printf("Sira No: %d\n", hastalar[i].siraNo);
+            printf("\n--- HASTA BILGILERI (HL7 FORMAT) ---\n");
+            hastaBilgisiHL7Format(i);
             bulundu = 1;
             break;
         }
@@ -254,25 +300,17 @@ void hastaBilgisiGoruntule() {
         printf("Hata: Bu TC No'ya ait hasta bulunamadi!\n");
     }
 }
-
 // Tum hastalari goruntuleme fonksiyonu
 void tumHastalariGoruntule() {
     int i;
     int aktifHastaSayisi = 0;
     
-    printf("\n=== TUM HASTALAR ===\n");
+    printf("\n=== TUM HASTALAR (HL7 FORMAT) ===\n");
     
     for (i = 0; i < hastaCount; i++) {
         if (hastalar[i].aktif) {
-            printf("\n--- %d. Hasta ---\n", ++aktifHastaSayisi);
-            printf("Ad Soyad: %s %s\n", hastalar[i].ad, hastalar[i].soyad);
-            printf("TC No: %s\n", hastalar[i].tcNo);
-            printf("Yas: %d, Cinsiyet: %c\n", hastalar[i].yas, hastalar[i].cinsiyet);
-            printf("Sikayet: %s\n", hastalar[i].sikayet);
-            printf("Triaj: %d\n", hastalar[i].triaj);
-            printf("Poliklinik: %s\n", hastalar[i].poliklinik);
-            printf("Doktor: %s\n", hastalar[i].doktor);
-            printf("Sira No: %d\n", hastalar[i].siraNo);
+            printf("\n--- %d. Hasta (HL7) ---\n", ++aktifHastaSayisi);
+            hastaBilgisiHL7Format(i);
         }
     }
     
@@ -280,7 +318,6 @@ void tumHastalariGoruntule() {
         printf("Sistemde kayitli hasta bulunmamaktadir.\n");
     }
 }
-
 // Hasta silme fonksiyonu
 void hastaSil() {
     char tcNo[12];
@@ -299,12 +336,10 @@ void hastaSil() {
             break;
         }
     }
-    
     if (!bulundu) {
         printf("Hata: Yanlis TC, kayit bulunamadi!\n");
     }
 }
-
 // Recete olusturma fonksiyonu
 void receteOlustur() {
     char tcNo[12];
@@ -335,5 +370,6 @@ void receteOlustur() {
     
     if (!bulundu) {
         printf("Hata: Bu TC No'ya ait hasta bulunamadi!\n");
+    
     }
 }
